a2410e0a4d0f7c7fb2536ae84798adce423feebd
Oliver Nicholls <omnicholls@yahoo.co.uk>
2024-09-17 11:33:02 -0400

    Updating next.py to include a demonstration of the StopIteration exception and how iterables work.

--- testing/next.py
+++ testing/next.py
@@ -1,6 +1,20 @@
 import numpy as np
+import random
 
-train_ds = iter(np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+# Demonstrating the use of the next() function
+data = iter(np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 for _ in range(10):
-    print(next(train_ds))
+    print(next(data))
+
+# Demonstrating the use of the StopIteration exception
+class RandomIterable:
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if random.choice(["go", "go", "stop"]) == "stop":
+            raise StopIteration  # signals "the end"
+        return 1
+
+for i in range(5):
+    print(list(RandomIterable()))
