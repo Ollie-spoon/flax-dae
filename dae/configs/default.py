629b10da3ce3f2fba6a0b35cf7c32919973a2dfd
Oliver Nicholls <omnicholls@yahoo.co.uk>
2024-09-24 13:56:56 -0400

    minor changes

--- dae/configs/default.py
+++ dae/configs/default.py
@@ -21,14 +21,14 @@ def get_config():
     """Get the default hyperparameter configuration."""
     config = ml_collections.ConfigDict()
 
-    config.learning_rate = 0.0001
-    config.latents = 50
-    config.hidden = 280
+    config.learning_rate = 0.00025
+    config.latents = 35
+    config.hidden = 120
     config.dropout_rate = 0.2
     config.io_dim = 95
-    config.batch_size = 1000
+    config.batch_size = 500
     config.epoch_size = 10000
-    config.num_epochs = 100
+    config.num_epochs = 80
     config.data_path = 'C:/Users/omnic/OneDrive/Documents/MIT/Programming/approximation_coefficients_dataset.npy'
     config.checkpoint_restore_path = ""
     return config
