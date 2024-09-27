ff39d531f417801e2c6ea414f538808592c5d0ba
Oliver Nicholls <omnicholls@yahoo.co.uk>
2024-09-27 00:18:55 -0400

    learning rate adjustment

--- dae/configs/default.py
+++ dae/configs/default.py
@@ -14,7 +14,7 @@ def get_config():
     config.io_dim = 68
     config.batch_size = 100
     config.epoch_size = 1000
-    config.num_epochs = 10000
+    config.num_epochs = 20000
     config.data_path = 'C:/Users/omnic/OneDrive/Documents/MIT/Programming/approximation_coefficients_dataset.npy'
     config.checkpoint_restore_path = ""
     return config
